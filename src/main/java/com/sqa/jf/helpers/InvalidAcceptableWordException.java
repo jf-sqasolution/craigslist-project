c339b8fbc51ca01f7a7f28e0e1780c135c08110c
Jean-francois Nepton <jeanmfrancois@hotmail.com>
2016-10-10 18:29:26 -0700

    Create RequestInput Helper Class

---
+++ src/main/java/com/sqa/jf/helpers/InvalidAcceptableWordException.java
@@ -0,0 +1,22 @@
+/**
+ * File Name: InvalidAcceptableWordException.java<br>
+ * LastName, FirstName<br>
+ * Java Boot Camp Exercise<br>
+ * Instructor: Jean-francois Nepton<br>
+ * Created: Oct 10, 2016
+ */
+package com.sqa.jf.helpers;
+
+/**
+ * InvalidAcceptableWordException //ADDD (description of class)
+ * <p>
+ * //ADDD (description of core fields)
+ * <p>
+ * //ADDD (description of core methods)
+ * 
+ * @author LastName, FirstName
+ * @version 1.0.0
+ * @since 1.0
+ */
+public class InvalidAcceptableWordException extends Exception {
+}
